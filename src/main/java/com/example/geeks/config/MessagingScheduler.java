e132f3eb93f381d9284ddafce79fb6eba521cfd2
bak3839 <bak3839@naver.com>
2024-01-18 13:30:02 +0900

    fix: 메세지 스케줄러 수정

--- src/main/java/com/example/geeks/config/MessagingScheduler.java
+++ src/main/java/com/example/geeks/config/MessagingScheduler.java
@@ -3,6 +3,7 @@ package com.example.geeks.config;
 import com.example.geeks.requestDto.ChatMessage;
 import com.example.geeks.responseDto.SendChatMessage;
 import com.example.geeks.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,10 +13,11 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class MessagingScheduler {
 
     private SimpMessagingTemplate messagingTemplate;
-    private ChatService chatService;
+    private final ChatService chatService;
 
     @Autowired
     public void setMessagingTemplate(SimpMessagingTemplate messagingTemplate) {
