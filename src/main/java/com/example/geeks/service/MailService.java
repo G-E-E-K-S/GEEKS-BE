ae1422fd441afb87b0be85dba94f5f2ffb74c351
bak3839 <bak3839@naver.com>
2024-01-27 03:38:10 +0900

    chore: 이메일 html 태그 수정

--- src/main/java/com/example/geeks/service/MailService.java
+++ src/main/java/com/example/geeks/service/MailService.java
@@ -28,40 +28,39 @@ public class MailService {
         // https://drive.google.com/uc?export=view&amp;id=1Sq-EX7P31_gwDoSmUh5SJDD-8z1dfhKq
 
         String msgg =
-                "<div style=\"width: 1280px; overflow: hidden;\">\n" +
-                        "   <div style=\"width: 700px;\">\n" +
-                        "       <img src=\"https://seumu-s3-bucket.s3.ap-northeast-2.amazonaws.com/banner_pattern.svg\"/>\n" +
-                        "   </div>\n" +
-                        "   <div style=\"margin: 93px 0px 0px 160px; width: 100%;\">\n" +
-                        "       <div style=\"color: #000; font-size: 34px; font-weight: 700; line-height: normal; letter-spacing: 0.085px; width: 700px;\">\n" +
-                        "            OFI 인증메일입니다\n" +
-                        "       </div>\n" +
-                        "       <div style=\"margin-top: 46px; color: #000; font-size: 16px; font-weight: 400; line-height: normal; letter-spacing: 0.04px; width: 100%;\">\n" +
-                        "           안녕하세요 OFI 인증메일입니다.<br>\n" +
-                        "           5분 이내에 이메일에 있는 인증코드를 입력해 주세요.\n" +
-                        "       </div>\n" +
-                        "       <div style=\"margin-top: 46px; color: #000; font-size: 36px; font-weight: 700; line-height: normal; text-decoration: underline; width: 700px;\">\n" +
-                        "            인증 코드: "+ePw+"\n" +
-                        "       </div>\n" +
-                        "       <div style=\"margin-top: 46px; color: #000; font-size: 16px; font-weight: 400; line-height: normal; letter-spacing: 0.04px; width: 100%;\">\n" +
-                        "           인증번호를 인증창에 직접 입력하시거나 인증키를 복사 후 회원가입 페이지의 입력창에 붙여넣기 해주세요.<br>\n" +
-                        "           타인에게 인증번호 유출을 주의해주세요.\n" +
-                        "       </div>\n" +
-                        "   </div>\n" +
-                        "   <div style=\"margin-top: 40px; bottom: 0; width: 700px; height: 100%; background: #F3EFF4; padding: 50px 614px 50px 164px; color:#929094; font-size: 12px; font-weight: 400; line-height: normal; letter-spacing: 0.048px;\"> " +
-                        "   본 메일은 발신전용이며, 문의에 대한 회신은 처리되지 않습니다.<br> " +
-                        "   (주)아웃핏인 | 주소: 충남 천안시 동남구 상명대로 & 서울특별시 종로구 홍지문2길 20 | 팀원: 김규민 박준형 안지수 오현식 이소윤 이수정<br><br> " +
-                        "   Copyright ©OFI Corp. All rights reserved. " +
-                        "   </div> " +
-                        "</div>";
+                "<!DOCTYPE html>\n" +
+                        "<html style=\"padding: 0;margin: 0; font-family: Pretendard;word-break: break-all;\">\n" +
+                        "  <head style=\"background-color: #fff4cd;height: 19vh;display: flex;justify-content: center;align-items: center;\">\n" +
+                        "    <meta charset=\"UTF-8\" />\n" +
+                        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
+                        "    <title>Email</title>\n" +
+                        "    <link rel=\"stylesheet\" href=\"./Email.css\" />\n" +
+                        "  </head>\n" +
+                        "  <header>\n" +
+                        "    <img class=\"logo\" src=\"https://seumu-s3-bucket.s3.ap-northeast-2.amazonaws.com/geeksLogo.svg\" />\n" +
+                        "  </header>\n" +
+                        "  <body>\n" +
+                        "    <div style=\"padding-left: 6.94vw;\">\n" +
+                        "      <div style=\"color: #525252;font-size: 24px;font-weight: 500;margin-top: 9.19vh;\">\n" +
+                        "        안녕하세요, 긱스에서 요청하신 인증번호를 보내드려요.\n" +
+                        "      </div>\n" +
+                        "      <div style=\"color: #333;font-size: 64px;font-weight: 500;line-height: normal;margin-top: 11.63vh;margin-bottom: 11.7vh;\">"+ ePw +"</div>\n" +
+                        "      <div style=\"color: #525252;font-size: 32px;font-style: normal;font-weight: 500;\">위 인증번호 4자리를 인증번호 입력창에 정확히 입력해 주세요.</div>\n" +
+                        "      <div style=\"color: #525252;font-size: 24px;font-style: normal;font-weight: 500;margin-top: 3.46vh;\">인증번호를 요청하지 않았을 경우 본 이메일을 무시해 주세요.</div>\n" +
+                        "    </div>\n" +
+                        "  </body>\n" +
+                        "  <footer>\n" +
+                        "    <div style=\"color: #525252;font-size: 24px;font-style: normal;font-weight: 500;margin-top: 3.46vh;\">인증번호를 요청하지 않았을 경우 본 이메일을 무시해 주세요.</div>\n" +
+                        "  </footer style=\"position: fixed;bottom: 0;width: 100%;background-color: #fff4cd;height: 15vh;padding-top: 5.37vh;padding-left: 6.94vw;\">\n" +
+                        "</html>\n";
 
         MimeMessage message = emailSender.createMimeMessage();
 
         message.addRecipients(Message.RecipientType.TO, to);//보내는 대상
-        message.setSubject("[OutFitIn] 회원가입 이메일 인증번호");//제목
+        message.setSubject("[GEEKS] 회원가입 이메일 인증번호");//제목
 
         message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("ofi1234@naver.com","OutFitIn"));
+        message.setFrom(new InternetAddress("ofi1234@naver.com","GEEKS"));
 
         return message;
     }
