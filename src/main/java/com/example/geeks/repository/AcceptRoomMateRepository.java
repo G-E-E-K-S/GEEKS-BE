62a5673a5b29995afa4f1c41a589d7c8ceb60484
bak3839 <bak3839@naver.com>
2024-02-02 03:05:55 +0900

    chore: 룸메이트 성사 되면 보낸 신청 받은 신청 전부 삭제 및 내 프로필에 내 룸메이트 정보 전송

--- src/main/java/com/example/geeks/repository/AcceptRoomMateRepository.java
+++ src/main/java/com/example/geeks/repository/AcceptRoomMateRepository.java
@@ -2,6 +2,14 @@ package com.example.geeks.repository;
 
 import com.example.geeks.domain.AcceptRoomMate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AcceptRoomMateRepository extends JpaRepository<AcceptRoomMate, Long> {
+
+    @Query("select ac from AcceptRoomMate ac " +
+            "where ac.accept.id = :userId or ac.sender.id = :userId")
+    Optional<AcceptRoomMate> findAcceptRoomMateOptional(@Param("userId") Long userId);
 }
