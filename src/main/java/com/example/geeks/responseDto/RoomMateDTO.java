ac1815e43e06d3203a42fc10ecbe91e3ae5fce0b
bak3839 <bak3839@naver.com>
2024-02-02 00:41:48 +0900

    feat: 룸메이트 수락 기능 구현

--- src/main/java/com/example/geeks/responseDto/RoomMateDTO.java
+++ src/main/java/com/example/geeks/responseDto/RoomMateDTO.java
@@ -2,12 +2,13 @@ package com.example.geeks.responseDto;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class RoomMateDTO {
+    private Long userId;
+
     private String nickname;
 
     private String major;
@@ -21,7 +22,8 @@ public class RoomMateDTO {
     private LocalDateTime createdDate;
 
     @Builder
-    public RoomMateDTO(String nickname, String major, String introduction, String photoName, int studentID, LocalDateTime createdDate) {
+    public RoomMateDTO(Long userId, String nickname, String major, String introduction, String photoName, int studentID, LocalDateTime createdDate) {
+        this.userId = userId;
         this.nickname = nickname;
         this.major = major;
         this.introduction = introduction;
